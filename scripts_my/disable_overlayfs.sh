9c3fff4751296373f9ac0ff0db9e02848a4ce0f2
zcz123333 <156064587+zcz123333@users.noreply.github.com>
2024-01-18 23:02:56 +0800

    Update disable_overlayfs.sh

--- scripts_my/disable_overlayfs.sh
+++ scripts_my/disable_overlayfs.sh
@@ -3,5 +3,5 @@
 #rm -rf scripts/sd-fuse/friendlywrt23/userdata.img
 cp scripts/sd-fuse/prebuilt/parameter-ext4.txt scripts/sd-fuse/prebuilt/parameter.template
 sed -i '/USERDATA_PARTITION_ADDR/d' scripts/sd-fuse/tools/generate-partmap-txt.sh
-sed -i '' friendlywrt/build_dir/target-aarch64_generic_musl/root-rockchip/lib/upgrade/platform.sh
+sed -i '/platform_copy_config/,+10 s/export_partdevice partdev 1/export_partdevice partdev 8/' friendlywrt/build_dir/target-aarch64_generic_musl/root-rockchip/lib/upgrade/platform.sh
 #sed -i '/TMPDIR=/a \\tsudo touch ${TMPDIR}/.ext4.resized' scripts/sd-fuse/tools/update_prebuilt.sh
