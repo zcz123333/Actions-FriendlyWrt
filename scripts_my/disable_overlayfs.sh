5b1f51fca3359cc999425329583489a66d8065b4
zcz123333 <zcz123333@gmail.com>
2024-02-04 10:36:28 +0800

    revert 'disable fsresize when build without overlayfs'

--- scripts_my/disable_overlayfs.sh
+++ scripts_my/disable_overlayfs.sh
@@ -5,4 +5,3 @@ cp scripts/sd-fuse/prebuilt/parameter-ext4.txt scripts/sd-fuse/prebuilt/paramete
 #sed -i '/USERDATA_PARTITION_ADDR/d' scripts/sd-fuse/tools/generate-partmap-txt.sh
 sed -i '/platform_copy_config/,+10 s/export_partdevice partdev [0-9]\+/export_partdevice partdev 8/' `find friendlywrt/ -follow -type f -path '*/lib/upgrade/platform.sh'`
 #sed -i '/TMPDIR=/a \\tsudo touch ${TMPDIR}/.ext4.resized' scripts/sd-fuse/tools/update_prebuilt.sh
-sed -i 's/#sudo touch/sudo touch/' scripts/sd-fuse/tools/update_prebuilt.sh
