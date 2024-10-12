9f5a521d2beccd47ad01ff4c0a87750b1eaa7d8a
zcz123333 <156064587+zcz123333@users.noreply.github.com>
2024-10-12 22:44:42 +0800

    remove emmc-tools for all cpu models

--- scripts_my/before_build_img.sh
+++ scripts_my/before_build_img.sh
@@ -2,7 +2,7 @@
 
 #remove emmc-tools
 rm -rf device/common/emmc-tools/
-sed -i '/device\/common\/emmc-tools/d' device/friendlyelec/rk3328/base.mk
+sed -i '/device\/common\/emmc-tools/d' `find device/friendlyelec/ -follow -type f -path '*/base.mk'`
 
 #fix flash.js
 cp ../patches_my/0002-fix-to-make-factory-reset-button-work-on-FriendlyWrt.patch device/common/src-patchs/23.05/feeds/luci/
