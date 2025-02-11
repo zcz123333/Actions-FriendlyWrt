ce87bdb2dfec1640093af012d7e6e9824c4c1a44
zcz123333 <156064587+zcz123333@users.noreply.github.com>
2025-02-11 20:07:26 +0800

    revert ip_addr.sh no use

--- patches_my/ip_addr.sh
+++ patches_my/ip_addr.sh
@@ -4,9 +4,7 @@ uci set network.lan.ipaddr='192.168.5.11'
 uci set network.lan.netmask='255.255.255.0'
 #uci set network.lan.gateway='192.168.5.1'
 #uci set network.lan.dns='192.168.5.1'
-uci set network.lan.gateway=''
-uci set network.lan.dns=''
 uci set network.lan.delegate='0'
 uci commit network
-uci set dhcp.lan.ignore='0'
-uci commit dhcp
+#uci set dhcp.lan.ignore='0'
+#uci commit dhcp
