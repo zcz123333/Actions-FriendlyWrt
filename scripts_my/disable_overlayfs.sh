5a34f99c4e0c1cea07e047dcad40a71ddad51eff
zcz123333 <156064587+zcz123333@users.noreply.github.com>
2025-06-09 16:45:26 +0800

    upgrade 24.10

--- scripts_my/disable_overlayfs.sh
+++ scripts_my/disable_overlayfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#rm -rf scripts/sd-fuse/friendlywrt23/userdata.img
+#rm -rf scripts/sd-fuse/friendlywrt24/userdata.img
 cp scripts/sd-fuse/prebuilt/parameter-plain.txt scripts/sd-fuse/prebuilt/parameter.template
 #sed -i '/USERDATA_PARTITION_ADDR/d' scripts/sd-fuse/tools/generate-partmap-txt.sh
 sed -i '/platform_copy_config/,+10 s/export_partdevice partdev [0-9]\+/export_partdevice partdev 8/' `find friendlywrt/ -follow -type f -path '*/lib/upgrade/platform.sh'`
